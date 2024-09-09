bdfa17c6b4b8fa06830b57ae371f4f5b831b4f57
JitenKhatri <jitenkhatri81@gmail.com>
2024-09-09 12:01:56 -0500

    Added 3 screen logic.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 Console.WriteLine("Hello, World!");
+
+ScheduleAppointment();
+
+void ScheduleAppointment()
+{
+    var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("AppointmentDetails.json")
+            .Build();
+    var formData = configuration.GetSection("FormData");
+    IWebDriver driver = new ChromeDriver();
+
+    driver.Navigate().GoToUrl("https://public.txdpsscheduler.com/");
+
+    driver.FindElement(By.XPath("//span[text()='English']")).Click();
+
+    driver.FindElement(By.XPath("//label[text()='Texas Card Number (DL, ID, EIC)']/following-sibling::input")).SendKeys(formData["TexasCardNumber"]);
+    driver.FindElement(By.XPath("//label[text()='First Name']/following-sibling::input")).SendKeys(formData["FirstName"]);
+    driver.FindElement(By.XPath("//label[text()='Last Name']/following-sibling::input")).SendKeys(formData["LastName"]);
+    driver.FindElement(By.XPath("//label[text()='Date of Birth (mm/dd/yyyy)']/following-sibling::input")).SendKeys(formData["DateOfBirth"]);
+    driver.FindElement(By.XPath("//label[text()='Last four of SSN']/following-sibling::input")).SendKeys(formData["LastFourSSN"]);
+    driver.FindElement(By.XPath("//span[text()='Log On']")).Click();
+
+    driver.FindElement(By.XPath("//span[text()='New Appointment']")).Click();
+
+    driver.FindElement(By.XPath("//span[text()='Apply for first time Texas DL/Permit']")).Click();
+}
